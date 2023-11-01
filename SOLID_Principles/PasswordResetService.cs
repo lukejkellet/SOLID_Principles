6965f98a2444955ab6e8eb16f162e52d4f074950
Luke Kellet <lukejameskellet@hotmail.com>
2023-11-01 16:17:03 +0000

    Add project files.

---
+++ SOLID_Principles/PasswordResetService.cs
@@ -0,0 +1,29 @@
+﻿using SOLID_Principles.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Principles
+{
+    internal class PasswordResetService
+    {
+        private readonly IPasswordResetEmailSender emailSender;
+
+        public PasswordResetService(IPasswordResetEmailSender emailSender)
+        {
+            this.emailSender = emailSender;
+        }
+
+        public void RequestPasswordReset(string username)
+        {
+            // Implement password reset logic
+            // ...
+
+            // Send a password reset email using the injected password reset email sender
+            emailSender.SendPasswordResetEmail("user@example.com", "ResetLink");
+        }
+    }
+
+}
---
+++ SOLID_Principles/UserRegistrationService.cs
@@ -0,0 +1,29 @@
+﻿using SOLID_Principles.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Principles
+{
+    internal class UserRegistrationService
+    {
+        private readonly IUserRegistrationEmailSender emailSender;
+
+        public UserRegistrationService(IUserRegistrationEmailSender emailSender)
+        {
+            this.emailSender = emailSender;
+        }
+
+        public void RegisterUser(string username, string password)
+        {
+            // Register the user in the database
+            // ...
+
+            // Send a user registration email using the injected email sender
+            emailSender.SendUserRegistrationEmail("user@example.com", username);
+        }
+    }
+
+}
